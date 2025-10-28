8779f30da285d79a661e5e557b723f796ca34fe1
Borja Rodrigo Garrido <borja.rodrigogarrido16@gmail.com>
2025-10-28 12:58:45 +0100

    Change Supabase URL and key for new project
    Updated Supabase project URL and key.

--- supabaseClient.js
+++ supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm'
 
-const SUPABASE_URL = "https://aczjgeleupwlvghjhkav.supabase.co";
-const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFjempnZWxldXB3bHZnaGpoa2F2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjA5NzgwODMsImV4cCI6MjA3NjU1NDA4M30.-hQdLNDMw-i4vCpt_OXnzYss5MtVrCu76FhMCPcZPhM";
+const SUPABASE_URL = "https://qoknmumhuqkdnpogvqbc.supabase.co";  // tu nuevo Project URL
+const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFva25tdW1odXFrZG5wb2d2cWJjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjEyNDYyMDMsImV4cCI6MjA3NjgyMjIwM30.XKTM4wRqqSP-StGB9uL75BLvhesQ6VfA2jxK8ces5uI";
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
